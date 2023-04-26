022f12e5f2ffe5d6fde15bb871291b4fdc94bdb6
Lucas-IQ21 <lucas.jarrosson@gmail.com>
2023-04-26 13:21:33 +0000

    26/04/2023

--- dashboard.php
+++ dashboard.php
@@ -5,29 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Lucas </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="icon" href="document/pico.png"> 
   </head>
   <body>
     <div class="container text-center">
         <div class="row">
-          <div class="col">
-          </div>
-          <div class="col">
-            <form action="lucas.php" method="post">
-                <div class="mb-1 mt-3">
-                  <label for="inputPrenom" class="form-label">Prénom</label>
-                  <input type="text" name="prenom" class="form-control" id="inputPrenom" aria-describedby="prenomHelp">
-                  <div id="prenomHelp" class="form-text">Veuillez entrer votre prénom.</div>
-                </div>
-                <div class="mb-3 mt-3">
-                  <label for="inputAge" class="form-label">Age</label>
-                  <input type="text" name="age" class="form-control" id="inputAge" aria-describedby="ageHelp">
-                  <div id="ageHelp" class="form-text">Veuillez entrer votre age.</div>
-                </div>
-                <button type="submit" class="btn btn-primary">Envoyer</button>
-              </form>
-          </div>
-          <div class="col">
-          </div>
+            <?php echo "Hello World" ?>
         </div>
       </div>
 
