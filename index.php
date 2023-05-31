a865bab9aa05efffc8351cf77c6d0ce877b94b93
Lucas-IQ21 <lucas.jarrosson@gmail.com>
2023-05-31 12:12:19 +0000

    ajout maquette page

--- index.php
+++ index.php
@@ -2,16 +2,13 @@
 <html>
 <head>
     <meta charset='utf-8'>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/.css">
     <title>Page Title</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link  rel ="icon" href="https://o.remove.bg/downloads/e4fd8485-fe21-4acb-a5f9-f1f014c1db2f/image-removebg-preview.png" type="image/png"> 
 </head>
 <body>
     <h1> Bienvenue sur l'outil Pico ! </h1>
-    <?php
-            include 'connexion.php';
-    ?>
 
 </body>
 </html>
